0f1217a41d134ade744f55a0a07c843054198452
pingfury <pingfury@outlook.com>
2024-12-12 16:12:59 +0800

    fix templates

--- mysite/mysite/static/js/mysite.js
+++ mysite/mysite/static/js/mysite.js
@@ -1,31 +1,29 @@
+function createCarousel(carousel) {
+  const carouselContainer = carousel
+  const carouselItems = carouselContainer.querySelectorAll('.carousel-item');
+  let currentIndex = 0;
+  const itemWidth = carouselItems[0].offsetWidth;
 
-const carousel = document.querySelector('.carousel');
-const carouselItems = document.querySelectorAll('.carousel-item');
-let currentIndex = 0;
+  function scrollToIndex(index) {
+    carouselContainer.scrollTo({
+      left: index * itemWidth,
+      behavior: 'smooth'
+    });
+  }
 
-function scrollToItem(index) {
-  const itemWidth = carouselItems[0].offsetWidth; // 获取单个item的宽度
-  carousel.scrollTo({
-    left: index * itemWidth,
-    behavior: 'smooth' // 可选：平滑滚动
-  });
-}
+  function autoScroll() {
+    currentIndex = (currentIndex + 1) % carouselItems.length;
+    scrollToIndex(currentIndex);
+  }
 
-function autoScroll() {
-  currentIndex = (currentIndex + 1) % carouselItems.length;
-  scrollToItem(currentIndex);
-}
-
-setInterval(autoScroll, 3000); // 每3秒滚动一次
-
-/*
-// 可选：添加事件监听器，在鼠标悬停时暂停自动滚动
-carousel.addEventListener('mouseover', () => {
-  clearInterval(autoScroll);
-});
-
-carousel.addEventListener('mouseout', () => {
   setInterval(autoScroll, 3000);
-});
 
- */
+  //carouselContainer.addEventListener('mouseover', () => clearInterval(autoScroll));
+  //carouselContainer.addEventListener('mouseout', () => setInterval(autoScroll, 3000));
+}
+
+const elements = document.querySelectorAll('.carousel');
+
+elements.forEach(element => {
+  createCarousel(element);
+});
