5b4f189e92c6fe175650756d6c5484ee9801dfca
pingfury <pingfury@outlook.com>
2024-12-16 15:16:15 +0800

    fix settings

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -2,6 +2,13 @@ from .base import *
 
 DEBUG = False
 
+SECRET_KEY = "django-insecure-b!34rzoofoc$f&juul8#!qlyv70330#pe@(r2y$@a(al=y#h3s"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 try:
     from .local import *
 except ImportError:
