66bffc43943b8efc71917f858115f285897f0a3c
pingfury <pingfury@outlook.com>
2024-12-16 20:04:00 +0800

    fix settings

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *
 
 DEBUG = False
@@ -8,6 +10,8 @@ SECRET_KEY = "django-insecure-b!34rzoofoc$f&juul8#!qlyv70330#pe@(r2y$@a(al=y#h3s
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") or []
 
 try:
     from .local import *
